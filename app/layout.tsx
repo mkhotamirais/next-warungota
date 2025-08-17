6b080272ebb3dcc9aedf632bd4e1ef5ca8207bff
mkhotamirais <mkhotamirais@gmail.com>
2025-08-17 09:04:37 +0700

    lagi

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,7 @@ import "./globals.css";
 import Header from "@/components/layouts/Header";
 import Footer from "@/components/layouts/Footer";
 import { content as c } from "@/lib/content";
-import { auth } from "@/auth";
-import { SessionProvider } from "next-auth/react";
+import { NextAuthProviders } from "@/components/NextAuthProvider";
 
 const { title, description } = c.home.hero;
 
@@ -16,20 +15,19 @@ const geistSans = Inter({
 
 export const metadata: Metadata = { title: { default: title, template: "%s - WarungOta" }, description };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
   return (
     <html lang="en">
       <body className={`${geistSans.variable} antialiased min-h-screen flex flex-col`}>
-        <SessionProvider session={session}>
+        <NextAuthProviders>
           <Header />
           <main className="flex-1">{children}</main>
           <Footer />
-        </SessionProvider>
+        </NextAuthProviders>
       </body>
     </html>
   );
