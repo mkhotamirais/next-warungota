3d1a3037ac345bf932869a00cc9f8b1597dcd228
mkhotamirais <mkhotamirais@gmail.com>
2025-08-21 13:26:44 +0700

    category juga pakai generate static params

--- app/blog/category/[slug]/page.tsx
+++ app/blog/category/[slug]/page.tsx
@@ -1,7 +1,12 @@
 import Hero from "@/components/sections/Hero";
-import { getBlogs } from "@/lib/data";
+import { getBlogCategories, getBlogs } from "@/lib/data";
 import React from "react";
 import List from "../../List";
+
+export const generateStaticParams = async () => {
+  const categories = await getBlogCategories();
+  return categories.map((category) => ({ slug: category.slug }));
+};
 
 export default async function BlogCategory({ params }: { params: Promise<{ slug: string }> }) {
   const categorySlug = (await params).slug;
