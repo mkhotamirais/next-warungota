d60053f53373cbe11b45057ec06dc01a7f987a2b
mkhotamirais <mkhotamirais@gmail.com>
2025-08-21 13:20:50 +0700

    generate static params

---
+++ app/blog/detail/[slug]/loading.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Loading() {
+  return <div className="py-16 text-center">Loading Detail Blog..</div>;
+}
--- app/blog/detail/[slug]/page.tsx
+++ app/blog/detail/[slug]/page.tsx
@@ -6,9 +6,13 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
 
+export const generateStaticParams = async () => {
+  const blogs = await getBlogs(10, undefined, undefined);
+  return blogs.map((blog) => ({ slug: blog.slug }));
+};
+
 export default async function BlogSlug({ params }: { params: Promise<{ slug: string }> }) {
   const slug = (await params).slug;
-
   const [blog, latestBlogs] = await Promise.all([getBlogBySlug(slug), getBlogs(8, slug)]);
 
   if (!blog || !latestBlogs?.length) return notFound();
