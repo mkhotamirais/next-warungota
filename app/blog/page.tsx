5c1d912b9dbee69d4e490af53eb87569e1c1e343
mkhotamirais <mkhotamirais@gmail.com>
2025-09-17 10:23:24 +0700

    katanya ngga ada masalah

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,17 +2,16 @@ import Hero from "@/components/sections/Hero";
 import React from "react";
 import { content as c } from "@/lib/content";
 import { getBlogs } from "@/actions/blog";
-// import Load from "@/components/fallbacks/Load";
-// import Pagination from "@/components/ui/Pagination";
+import Pagination from "@/components/ui/Pagination";
 import AsideBlogCategory from "@/components/sections/AsideBlogCategory";
 import List from "./List";
 
 const { title, description } = c.blog;
 
-export default async function Blog() {
-  // const page = Number((await searchParams).page || 1);
+export default async function Blog({ searchParams }: { searchParams: Promise<{ page?: string }> }) {
+  const page = Number((await searchParams).page || 1);
 
-  const { blogs } = await getBlogs({ page: 1, limit: 3 });
+  const { blogs, totalPages } = await getBlogs({ page: page || 1, limit: 3 });
 
   return (
     <>
@@ -23,7 +22,7 @@ export default async function Blog() {
             {blogs?.length ? (
               <>
                 <List blogs={blogs} />
-                {/* <Pagination totalPages={totalPages} /> */}
+                <Pagination totalPages={totalPages} />
               </>
             ) : (
               <h2 className="h2">No Blog Found</h2>
