770bec636bf7ab70c7317bc3793c88a1657977fd
mkhotamirais <mkhotamirais@gmail.com>
2025-10-06 15:23:46 +0700

    masih cari server nya

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,19 @@
-// import { getBlogs } from "@/actions/blog";
-// import { getProducts } from "@/actions/product";
-// import HomeBlog from "@/components/sections/HomeBlog";
-// import HomeHero from "@/components/sections/HomeHero";
-// import HomeProduct from "@/components/sections/HomeProduct";
+import { getBlogs } from "@/actions/blog";
+import { getProducts } from "@/actions/product";
+import HomeBlog from "@/components/sections/HomeBlog";
+import HomeHero from "@/components/sections/HomeHero";
+import HomeProduct from "@/components/sections/HomeProduct";
 import React from "react";
 
 export default async function Home() {
-  // const { products } = await getProducts({ limit: 4 });
-  // const { blogs } = await getBlogs({ limit: 4 });
+  const { products } = await getProducts({ limit: 4 });
+  const { blogs } = await getBlogs({ limit: 4 });
 
   return (
     <>
-      halo
-      {/* <HomeHero />
+      <HomeHero />
       <HomeProduct products={products} />
-      <HomeBlog blogs={blogs} /> */}
+      <HomeBlog blogs={blogs} />
     </>
   );
 }
