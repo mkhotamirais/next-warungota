ec8e070c03856d3f7068c95864065da096720afc
mkhotamirais <mkhotamirais@gmail.com>
2025-10-06 15:19:54 +0700

    cek header

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
-import { getBlogs } from "@/actions/blog";
-import { getProducts } from "@/actions/product";
-import HomeBlog from "@/components/sections/HomeBlog";
-import HomeHero from "@/components/sections/HomeHero";
-import HomeProduct from "@/components/sections/HomeProduct";
+// import { getBlogs } from "@/actions/blog";
+// import { getProducts } from "@/actions/product";
+// import HomeBlog from "@/components/sections/HomeBlog";
+// import HomeHero from "@/components/sections/HomeHero";
+// import HomeProduct from "@/components/sections/HomeProduct";
 import React from "react";
 
 export default async function Home() {
-  const { products } = await getProducts({ limit: 4 });
-  const { blogs } = await getBlogs({ limit: 4 });
+  // const { products } = await getProducts({ limit: 4 });
+  // const { blogs } = await getBlogs({ limit: 4 });
 
   return (
     <>
-      <HomeHero />
+      halo
+      {/* <HomeHero />
       <HomeProduct products={products} />
-      <HomeBlog blogs={blogs} />
+      <HomeBlog blogs={blogs} /> */}
     </>
   );
 }
