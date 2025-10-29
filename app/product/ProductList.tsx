654d13bcc2ea422e387230350a93643ec6b2e513
mkhotamirais <mkhotamirais@gmail.com>
2025-10-29 15:27:20 +0700

    alag

--- app/product/ProductList.tsx
+++ app/product/ProductList.tsx
@@ -18,9 +18,9 @@ export default async function ProductList({
   page,
   keyword,
   categorySlug,
-  sortPrice,
-  minPrice,
-  maxPrice,
+  // sortPrice,
+  // minPrice,
+  // maxPrice,
 }: ProductListProps) {
   const { products, totalPages, totalProductsCount } = await getProducts({
     page,
@@ -29,34 +29,34 @@ export default async function ProductList({
     categorySlug,
   });
 
-  let appliedProducts = products;
+  // let appliedProducts = products;
 
-  const productsWithMinPrice = products.map((product) => ({
-    ...product,
-    minPrice: product.ProductVariant.reduce((min, variant) => Math.min(min, variant.price), Infinity),
-  }));
+  // const productsWithMinPrice = products.map((product) => ({
+  //   ...product,
+  //   minPrice: product.ProductVariant.reduce((min, variant) => Math.min(min, variant.price), Infinity),
+  // }));
 
-  if (sortPrice) {
-    appliedProducts =
-      sortPrice === "asc"
-        ? productsWithMinPrice.sort((a, b) => a.minPrice - b.minPrice)
-        : productsWithMinPrice.sort((a, b) => b.minPrice - a.minPrice);
-  } else {
-    appliedProducts = products;
-  }
+  // if (sortPrice) {
+  //   appliedProducts =
+  //     sortPrice === "asc"
+  //       ? productsWithMinPrice.sort((a, b) => a.minPrice - b.minPrice)
+  //       : productsWithMinPrice.sort((a, b) => b.minPrice - a.minPrice);
+  // } else {
+  //   appliedProducts = products;
+  // }
 
-  if (minPrice && maxPrice) {
-    appliedProducts = productsWithMinPrice.filter(
-      (product) => product?.minPrice >= Number(minPrice) && product.minPrice <= Number(maxPrice)
-    );
-  }
+  // if (minPrice && maxPrice) {
+  //   appliedProducts = productsWithMinPrice.filter(
+  //     (product) => product?.minPrice >= Number(minPrice) && product.minPrice <= Number(maxPrice)
+  //   );
+  // }
 
   return (
     <div>
       {products?.length ? (
         <>
           <div className="grid-all-list">
-            {appliedProducts.map((item, i) => (
+            {products.map((item, i) => (
               <ProductCard key={i} item={item} />
             ))}
           </div>
